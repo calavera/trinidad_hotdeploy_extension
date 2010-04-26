a38716cd78a1deb534e24010d8957e27f9e9069c
David Calavera <david.calavera@gmail.com>
2010-04-26 20:47:25 +0200

    initial commit

---
+++ src/main/java/org/jruby/trinidad/HotDeployLifecycleListener.java
@@ -0,0 +1,42 @@
+package org.jruby.trinidad;
+
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+
+class HotDeployLifecycleListener implements LifecycleListener {
+
+    private final Context applicationContext;
+    private final String monitor;
+    private final long delay;
+
+    public HotDeployLifecycleListener(Context applicationContext, String monitor, long delay) {
+        this.applicationContext = applicationContext;
+        this.monitor = new File(monitor).getAbsolutePath();
+        this.delay = delay;
+    }
+
+    @Override
+    public void lifecycleEvent(LifecycleEvent event) {
+        if (Lifecycle.BEFORE_START_EVENT.equals(event.getType())) {
+            init();
+        }
+    }
+
+    private void init() {
+        HotDeployObserver.observe(applicationContext, monitor, delay);
+
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+                //stop the observer
+            }
+        });
+    }
+}
---
+++ src/main/java/org/jruby/trinidad/HotDeployObserver.java
@@ -0,0 +1,97 @@
+package org.jruby.trinidad;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleException;
+
+public class HotDeployObserver extends Thread {
+    private final Context applicationContext;
+    private final File monitor;
+    private final long delay;
+
+    private boolean interrupted;
+    private long lastModified;
+
+    public HotDeployObserver(Context applicationContext, String monitor, long delay) {
+        super("TrinidadHotDeployerObserver");
+        this.applicationContext = applicationContext;
+        this.monitor = new File(monitor);
+        this.delay = delay;
+        setDaemon(true);
+    }
+
+    public static void observe(Context applicationContext, String monitor, long delay) {
+        HotDeployObserver observer = new HotDeployObserver(applicationContext, monitor, delay);
+        observer.createMonitor();
+        observer.start();
+    }
+
+    public void createMonitor() {
+        try {
+            monitor.createNewFile();
+            lastModified = monitor.lastModified();
+        } catch (IOException io) {
+            System.err.println("Was not allowed to create the monitor: " + monitor.getAbsolutePath());
+        }
+    }
+
+    public void observeAndRestart() {
+        boolean monitorExists;
+        try {
+            monitorExists = monitor.exists();
+        } catch(SecurityException  e) {
+            System.err.println("Was not allowed to check monitor existance: " + monitor.getAbsolutePath());
+            interrupted = true;
+            return;
+        }
+
+        if (monitorExists) {
+            long last = monitor.lastModified();
+            if (last > lastModified) {
+                lastModified = last;
+                System.out.println("reloading " + applicationContext.getName());
+                restartApplicationContext();
+            }
+        } else {
+            createMonitor();
+        }
+    }
+
+    public void run() {    
+        while(!interrupted) {
+          try {
+            Thread.sleep(delay);
+          } catch(InterruptedException e) {
+          }
+          observeAndRestart();
+        }
+    }
+
+    private void restartApplicationContext() {
+        try {
+            Map<String, String> parameters = new HashMap<String, String>();
+            for (String parameter : applicationContext.findParameters()) {
+                parameters.put(parameter, applicationContext.findParameter(parameter));
+            }
+
+            System.out.println("stoping " + applicationContext.getName());
+            ((Lifecycle)applicationContext).stop();
+
+            for (Map.Entry<String, String> entry : parameters.entrySet()) {
+                applicationContext.addParameter(entry.getKey(), entry.getValue());
+            }
+
+            System.out.println("starting " + applicationContext.getName());
+            ((Lifecycle)applicationContext).start();
+        } catch (LifecycleException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
