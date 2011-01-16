111d901582c1c2bdde662f684d2cf0f447117b22
David Calavera <david.calavera@gmail.com>
2011-01-16 15:07:27 +0100

    Release 0.4.1

--- lib/trinidad_hotdeploy_extension.rb
+++ lib/trinidad_hotdeploy_extension.rb
@@ -3,7 +3,7 @@ module Trinidad
     require File.expand_path('../../trinidad-libs/trinidad-hotdeploy-extension.jar', __FILE__)
 
     module Hotdeploy
-      VERSION = '0.4.0'
+      VERSION = '0.4.1'
     end
 
     class HotdeployWebAppExtension < WebAppExtension
